e278992e3532bf20cfd8777e5cd0237757635a63
kote <gvarjaladzekote@gmail.com>
2021-04-09 23:10:05 +0400

    myWorks added

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./components/navbar/navbar";
 import Header from "./components/header/header";
 import AboutMe from "./components/About me/AboutMe";
+import MyWork from "./components/work/myWork";
 const App: React.FC = (): JSX.Element => {
   return (
     <div className="App">
       <Navbar />
       <Header />
       <AboutMe />
+      <MyWork />
     </div>
   );
 };
---
+++ src/components/work/myWork.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import "./style.css";
+import firstProject from "../../images/firstProject.jpg";
+import recipeApp from "../../images/recipeApp.jpg";
+import quizApp from "../../images/quiz-app.png";
+import Image from "react-bootstrap/Image";
+import * as Bootsrap from "react-bootstrap";
+
+interface Works {
+  href: string;
+  photo: any;
+  name: string;
+}
+const MyWork: React.FC = (): JSX.Element => {
+  const Works = ({ href, photo, name }: Works): JSX.Element => {
+    return (
+      <div className="text-center">
+        <div className="d-flex justify-content-center">
+          <a href={href}>
+            <Bootsrap.Col xs={10} md={12} className="work-photo">
+              <Image src={photo} fluid />
+            </Bootsrap.Col>
+            <p>{name}</p>
+          </a>
+        </div>
+        <p className="made-with">made with react</p>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <div className="my-work-header">
+        <h1> my works</h1>
+        <p>Check out my lastest projects</p>
+        <hr />
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm">
+            <Works
+              href="https://serene-leavitt-ee9476.netlify.app"
+              photo={firstProject}
+              name="Todo app"
+            />
+          </div>
+          <div className="col-sm">
+            <Works
+              href="https://pedantic-mirzakhani-0d4030.netlify.app"
+              photo={recipeApp}
+              name="Recipe App"
+            />
+          </div>
+          <div className="col-sm">
+            <Works
+              href="https://zen-mcclintock-9d836e.netlify.app"
+              photo={quizApp}
+              name="Quiz App"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default MyWork;
